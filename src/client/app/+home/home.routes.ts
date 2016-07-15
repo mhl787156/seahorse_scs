5d047275a4022229a0059ed096f22a4cd54ff546
mhl787156 <Mickeyhli@outlook.com>
2016-07-15 17:16:06 +0100

    Added authentication to more pages

--- src/client/app/+home/home.routes.ts
+++ src/client/app/+home/home.routes.ts
@@ -1,17 +1,14 @@
 import { RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './index';
-import { AuthGuardService } from '../shared/index';
 
 export const HomeRoutes: RouterConfig = [
   {
     path: 'home',
     component: HomeComponent,
-    canActivate: [AuthGuardService]
   },
   {
     path: '',
     component: HomeComponent,
-    canActivate: [AuthGuardService]
   }
 ];
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,19 +1,33 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
+import {AuthGuardService} from './shared/index';
+
 import { CustomersRoutes } from './+customers/index';
 import { HomeRoutes } from './+home/index';
 import { OrdersRoutes } from './+orders/index';
 import { AdminRoutes } from './+admin/index';
 import { LoginRoutes } from './login/index';
 
-const routes: RouterConfig = [
+const loginroutes: RouterConfig = [
   ...HomeRoutes,
   ...CustomersRoutes,
   ...AdminRoutes,
-  ...OrdersRoutes,
+  ...OrdersRoutes
+];
+
+const routes: RouterConfig = [
   ...LoginRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
-  provideRouter(routes),
+  provideRouter(makeRoutes()),
 ];
+
+function makeRoutes() : RouterConfig {
+  let lr = loginroutes.map(conf => {
+              conf.canActivate = [AuthGuardService];
+              return conf;
+           });
+  return routes.concat(lr);
+}
+
